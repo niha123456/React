a3fa8263fa676ffa516efa9130137b388923146f
niha123456 <139298067+niha123456@users.noreply.github.com>
2024-04-03 16:15:06 +0530

    react-hook-form example

---
+++ react-practice/src/Components/reacthookform/reacthookform.jsx
@@ -0,0 +1,42 @@
+import { useForm } from 'react-hook-form';
+import './reacthookform.css';
+export function HookFrormDemo() {
+    const{register,handleSubmit,formState:{errors}} = useForm();
+    const submit = (values)=>{
+        console.log(values);
+    }
+
+    return(
+        <div className="container">
+            
+           <form onSubmit={handleSubmit(submit)}>
+                <h3>User-Registration</h3>
+            <dl>
+                    <dt>UserName</dt>
+                    <dd><input type="text" name='userName' className='form-control' {...register("userName",{required:true,minLength:4})} /></dd>
+                    <dd className='text-danger'>
+                        {
+                            (errors.userName?.type==="required")?<span>Name Required</span>:<span></span> && (errors.userName?.type==="minLength")?<span>Name too short..-Min 4 Chars are required</span>:<span></span>
+        
+                        }
+                    </dd>
+                    <dt>Mobile</dt>
+                    <dd><input type="number" name='mobile' className='form-control' {...register("mobile",{required:true,pattern:/\+91\d{10}/})} /></dd>
+                    <dd className='text-danger'>
+                        {
+                            (errors.mobile?.type==="required")?<span>Mobile Required</span>:<span></span>
+                        }
+                    </dd>
+                    <dt>Age</dt>
+                    <dd><input type="number" name='age' className='form-control' {...register("age",{required:true})} /></dd>
+                    <dd className='text-danger'>
+                        {
+                            (errors.age?.type==="required")?<span>Age Required</span>:<span></span>
+                        }
+                    </dd>
+                </dl>
+                <button type = "submit" className='btn btn-primary progress-bar-striped'>Register</button>
+           </form>
+        </div>
+    )
+}
--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -5,14 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Login } from './Components/login/login';
 import { Signin } from './Components/signin/signin';
+
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import * as bootstrap from "bootstrap";
+import { HookFrormDemo } from './Components/reacthookform/reacthookform';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    < Signin/>
+    < HookFrormDemo/>
   </React.StrictMode>
 );
 
